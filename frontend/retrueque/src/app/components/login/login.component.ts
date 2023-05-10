3ccc11c7ddc90242afa1449f478ed3c8f9d31994
MontoyaPablo <montoya_pablo@outlook.com.ar>
2023-05-10 18:14:28 -0300

    login popup

---
+++ frontend/retrueque/src/app/components/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent {
+    showPopup = false;
+
+    login() {
+      console.log("funca");
+    }
+
+    close() {
+      this.showPopup = false;
+      console.log("tmb funca");
+    }
+}
+
